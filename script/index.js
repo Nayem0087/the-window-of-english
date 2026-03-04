c2eb7b3aa4000ceda969f0b76c1cc60b36310a17
Nayem Ahmed <nayemk0087@gmail.com>
2026-03-04 16:08:01 +0600

    added my vocabulary compelte section

--- script/index.js
+++ script/index.js
@@ -13,23 +13,42 @@ const loadLevelWord = (id) => {
         .then((data) => displayLevelWord(data.data))
 };
 
-    const displayLevelWord = (words) => {
-        // console.log(words);
-        // 1. get data & empty
-        const wordContainer = document.getElementById('word-container');
-        wordContainer.innerHTML = '';
-        // 2. get into every lessons
-        words.forEach(word => {
-            console.log(word); 
-            // 3. create card
-            const card = document.createElement('div');
-            card.innerHTML = `
-                <p>Cat</p>
+const displayLevelWord = (words) => {
+    // console.log(words);
+    // 1. get data & empty
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = '';
+
+    if (words.length == 0) {
+        wordContainer.innerHTML = `
+        <div class="text-center col-span-full rounded-xl py-10 space-y-6">
+            <img class="mx-auto" src="./assets/alert-error.png" />
+            <p class="font-bangla text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bangla font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+        `;
+        return;
+    }
+    // 2. get into every lessons
+    words.forEach(word => {
+        console.log(word);
+        // 3. create card
+        const card = document.createElement('div');
+        card.innerHTML = `
+                <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায় নি"}</h2>
+            <p class="font-semibold">Meaning /Pronunciation</p>
+            <div class="font-bangla font-medium text-2xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি" } "</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
             `;
-            // 4. append card
-            wordContainer.append(card);
-        });
-    };
+        // 4. append card
+        wordContainer.append(card);
+    });
+};
 
 const displayLesson = (lessons) => {
     // console.log(lessons);
