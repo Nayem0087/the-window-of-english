101a0d3ab3c596f24cee63d6582574800013713c
Nayem Ahmed <nayemk0087@gmail.com>
2026-03-05 13:00:34 +0600

    added my complete modal section

--- script/index.js
+++ script/index.js
@@ -1,3 +1,10 @@
+const createElements = (arr) => {
+    // console.log(arr);
+    const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+    // console.log(htmlElements.join(" "));
+     return htmlElements.join(" ");
+}
+
 const loadLessons = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all') // promise of response
         .then((res) => res.json()) // promise of json data
@@ -25,6 +32,41 @@ const loadLevelWord = (id) => {
         });
 };
 
+const loadWordDetail = async (id) => {
+    const url = `
+        https://openapi.programming-hero.com/api/word/${id}
+    `;
+    // console.log(url);
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetails(details.data);
+};
+
+const displayWordDetails = (word) => {
+    console.log(word);
+    const detailsBox = document.getElementById('details-container');
+    detailsBox.innerHTML = `
+         <div class="">
+                <h2 class="text-2xl font-bold">
+                ${word.word} (<i class="fa-solid fa-microphone-lines"></i> : 
+                ${word.pronunciation})</h2>
+            </div>
+            <div class="">
+                <h2 class="font-bold">Meaning</h2>
+                <p>${word.meaning}</p>
+            </div>
+            <div class="">
+                <h2 class="font-bold">Example</h2>
+                <p>${word.sentence}</p>
+            </div>
+            <div class="">
+                <h2 class="font-bold">Synonym</h2>
+            <div class="">${createElements(word.synonyms)}</div>
+            </div>
+    `;
+    document.getElementById('word_modal').showModal();
+}
+
 const displayLevelWord = (words) => {
     // console.log(words);
     // 1. get data & empty
@@ -50,9 +92,9 @@ const displayLevelWord = (words) => {
                 <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
             <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায় নি"}</h2>
             <p class="font-semibold">Meaning /Pronunciation</p>
-            <div class="font-bangla font-medium text-2xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি" } "</div>
-            <div onclick="my_modal_5.showModal()" class="flex justify-between items-center">
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+            <div class="font-bangla font-medium text-2xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি"} "</div>
+            <div class="flex justify-between items-center">
+                <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
